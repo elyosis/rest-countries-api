2e2f921076e6241a8f8a917e8cec59b62700715f
Antía <elyosis@proton.me>
2024-01-06 00:03:25 +0100

    Added Countries module

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegionSelectComponent } from './components/region-select/region-select.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    RegionSelectComponent,
+    SearchBarComponent
   ],
   imports: [
     CommonModule,
