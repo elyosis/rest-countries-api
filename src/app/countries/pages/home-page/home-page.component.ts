a8c760c5e4b7ff5162e0643ce24d9a5167e2b997
Antía <elyosis@proton.me>
2024-01-08 18:38:03 +0100

    Added dynamic country cards generation on homepage

--- src/app/countries/pages/home-page/home-page.component.ts
+++ src/app/countries/pages/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) { }
+
+  ngOnInit(): void {
+    this.countriesService.searchCountries().subscribe(countries => this.countries = countries)
+  }
 
 }
