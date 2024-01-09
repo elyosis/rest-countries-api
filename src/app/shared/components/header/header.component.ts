7ab6eeb3574d6f6741a3cb72315e10309d155b0e
Antía <elyosis@proton.me>
2024-01-09 23:37:34 +0100

    Implemented saving color theme in localStorage

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  currentTheme: "light" | "dark" = "dark";
+export class HeaderComponent implements OnInit {
+  currentTheme?: "light" | "dark";
+
+  ngOnInit(): void {
+    this.currentTheme = this.calculatePreferredTheme();
+    document.body.setAttribute("data-theme", this.currentTheme);
+  }
+
+  calculatePreferredTheme() {
+    const currentTheme = localStorage.getItem("color-theme");
+    const systemSettingLight = window.matchMedia("(prefers-color-scheme: light)")
+
+    if (currentTheme === "light" || currentTheme === "dark") {
+      return currentTheme;
+    } else if (systemSettingLight.matches) {
+      return "light";
+    } else {
+      return "dark";
+    }
+  }
 
   changeTheme(): void {
     this.currentTheme === "light" ? this.currentTheme = "dark" : this.currentTheme = "light";
 
     document.body.setAttribute('data-theme', this.currentTheme);
+    localStorage.setItem("color-theme", this.currentTheme);
   }
 
 }
