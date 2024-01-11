b255169fd50093b94ec8d9f6b73d3443117bffdc
Antía <elyosis@proton.me>
2024-01-11 23:34:24 +0100

    Added border countriers to individual pages

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CountriesService } from '../../services/countries.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, forkJoin } from 'rxjs';
 import { Country } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-country-page',
@@ -9,21 +10,37 @@ import { Country } from '../../interfaces/country.interface';
   styleUrls: ['./country-page.component.css']
 })
 export class CountryPageComponent implements OnInit {
-  @Input() code!: string;
-  country?: Country;
+  code!: string;
+  country!: Country | null;
+  borderCountries?: { name: string, code: string }[];
   isLoading: boolean = false;
 
-  constructor(private router: Router, private countriesService: CountriesService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private countriesService: CountriesService) { }
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.countriesService.searchCountryByCode(this.code).subscribe((country) => {
-      if (!country) {
-        this.isLoading = false;
-        return this.router.navigateByUrl("")
-      };
-      this.isLoading = false;
-      return this.country = country;
+
+    this.route.params.subscribe(params => {
+      this.code = params['code'];
+
+      forkJoin([this.countriesService.searchCountryByCode(this.code),
+      this.countriesService.sendRequest("https://restcountries.com/v3.1/all").pipe(
+        map((countries) => {
+          return countries.map(country => { return { name: country.name.common, code: country.cca3 } })
+        })
+      )]).subscribe((result) => {
+        const country = result[0];
+
+        if (!country) {
+          return this.router.navigateByUrl("")
+        };
+
+        this.country = country;
+        this.borderCountries = result[1].filter(country => this.country?.borders?.includes(country.code));
+        return;
+      })
+
     })
+    this.isLoading = false;
   }
 }
